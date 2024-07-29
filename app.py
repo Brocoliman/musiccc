c0b1d0b03b5e64569b7bab96d892038c0f115b7e
AsiansNextDoor <yinzixuansz@gmail.com>
2024-07-29 11:03:50 -0700

    Imported Flask

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import (
+    Flask,
+    make_response,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    abort,
+)
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/playlists")
+def playlists():
+    return render_template("playlists.html")
--- assets/js/templates.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    $('.navbar').load('assets/templates/nav2.html');
-})
